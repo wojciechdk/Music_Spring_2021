69522e118d8c132f3d322346c25e38945bd28cc1
s001284 <s001284@student.dtu.dk>
2021-01-18 15:41:39 +0100

    started documenting in notebook

--- clean_data.py
+++ clean_data.py
@@ -14,6 +14,7 @@ path_df_1E6 = Config.Path.project_data_root / 'df_sample_1E6.parquet'
 df_1E5 = t.spark.load_dataframe_from_parquet(path_df_1E5, spark)
 df_1E6 = t.spark.load_dataframe_from_parquet(path_df_1E6, spark)
 
+
 #%% Cache
 df_1E5.cache()
 df_1E6.cache()
@@ -23,6 +24,36 @@ df_1E6.cache()
 number_of_rows_df_1E5 = t.spark.count_rows(df_1E5)
 number_of_rows_df_1E6 = t.spark.count_rows(df_1E6)
 
-#%%
 
+#%% Convert to pandas
+df_sample_pd = df_1E5.limit(100).toPandas()
+
+
+
+
+
+#%%
+# Compute dataframe with number of listings and neighbourhoods per city.
+# Order by number of listings (bigger first).
+df_listings_neighborhoods_per_city = (
+    df_1E5
+    .where(f.col("neighbourhood").isNotNull()) # Disregard rows where neighbourhood is not specified.
+    .groupBy('city')
+    .agg(
+        f.countDistinct('id').alias('number_of_listings'),  # Count the number of listings (IDs)
+        f.countDistinct('neighbourhood').alias('number_of_neighbourhoods')  # Count the number of neighborhoods
+        )
+    .orderBy(f.desc('number_of_listings'))
+    )
+
+
+#%%
+data_root, data_subfolders, data_files = t.get_data_paths()
+
+with open(data_files[0], 'rb') as file:
+    avro_reader = fastavro.reader(file)
+
+    column_docs = t.get_avro_docs(avro_reader.writer_schema)
+
+print(column_docs)
 
--- toolbox/imports.py
+++ toolbox/imports.py
@@ -14,6 +14,7 @@ import pyspark
 import os
 import toolbox as t
 import time
+import wojciech as w
 
 from pathlib import Path
 from pyspark.sql import SparkSession
