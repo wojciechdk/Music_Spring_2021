ac999dabaee54c09c1380db370c2bc3c9d32619e
s001284 <s001284@student.dtu.dk>
2021-01-20 11:25:45 +0100

    nothing

--- toolbox/imports.py
+++ toolbox/imports.py
@@ -8,6 +8,7 @@ import toolbox as t
 import time
 import wojciech as w
 
+from IPython.core.interactiveshell import InteractiveShell
 from pathlib import Path
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
