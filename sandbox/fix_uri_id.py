f17689c2df0f58dc5f0676b2d5724f83f4764769
s001284 <s001284@student.dtu.dk>
2021-07-05 20:48:30 +0200

    pre_processed_database_table.tex: up to end, needs verification

---
+++ sandbox/fix_uri_id.py
@@ -0,0 +1,37 @@
+from toolbox.initialize import *
+
+
+#%% Initialize a Spark session.
+
+# Save the start time for timing.
+start_time = time.time()
+
+#
+spark = t.spark.create_session('Music_Activity')
+
+# Set this for faster conversion from Spark to pandas.
+spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+
+
+# Define the path to the data.
+data_path = Config.Path.music_data_clean_root
+
+# Load the data into a clean dataframe
+df = t.load_data_from_files(data_path,
+                            spark,
+                            method='parquet')
+
+
+# Save the dataframe with partitions defined by the first
+# two letters of the user ID.
+(df
+ # .withColumn('user_id_prefix', f.col('user_id').substr(0, 2))
+ .withColumnRenamed('track_id', 'spotify_uri')
+ .withColumnRenamed('track_uri', 'sony_uri')
+ .write.mode('overwrite')
+ .partitionBy('user_id_prefix')
+ .parquet(str(Config.Path.project_data_root / 'df_clean_uri'))
+)
+
+# Print the execution time.
+print(f'Execution time: {time.time() - start_time:.5f} s.')
